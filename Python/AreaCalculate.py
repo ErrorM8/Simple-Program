379179a0fd76783b02ae45d1dac36685f0a20bed
Sakibul Islam Sakif <islamsakifbul@gmail.com>
2024-10-17 10:07:29 -0600

    Update and rename tem to AreaCalculate.py

---
+++ Python/AreaCalculate.py
@@ -0,0 +1,18 @@
+# Circle 
+r = float(input("Enter the Redius :"))
+area1 = 3.1416 * r
+print("Area of circle = ",area1)
+
+# Triangle
+b = float(input("Enter the Base :"))
+h = float(input("Enter the Hight :"))
+area2 = 0.5 * b * h
+print("Area of triangle = ",area2)
+
+# Trapezium
+a = input("Enter the Length: ")
+b = input("Enter the Wide: ")
+h = float(input("Enter the Hight :"))
+sum = int(a) + int(b)
+area3 = 0.5 * sum * h
+print("Area of Trapezium = ",area3)
